e5448d697ed04fde34dec76fef47faa162db406f
kelicaleb <kelicaleb7@gmail.com>
2022-08-23 15:15:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = ()=>{
+    return "bob"
+}
+function upperCaseCustomerName(){
+    let customer = customerName();
+    console.log(customer.toUpperCase());
+};
+upperCaseCustomerName();
+function setBestCustomer(){
+    let customer = customerName();
+    return "not" + customer;
+}
+setBestCustomer();
+function overwriteBestCustomer(){
+    let customer = setBestCustomer();
+    let overwrite = customer.slice(3, 6);
+    console.log(overwrite)
+    
+}
+overwriteBestCustomer();
+const leastFavoriteCustomer = "Caleb";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Keli";
+    return leastFavoriteCustomer
+
+}
+changeLeastFavoriteCustomer();
